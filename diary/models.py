8ab1d1ff0f63bac5ef5e3cc660053f7b93187622
jeeyunlee <jylee@kittenpla.net>
2017-08-15 14:57:02 +0900

    view, model까지 연동

--- diary/models.py
+++ diary/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Posting(models.Model):
+    text = models.CharField(max_length = 500)
+    update_date = models.DateField()
--- diary/views.py
+++ diary/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Posting
 
 # Create your views here.
+def index(request):
+    postings = Posting.objects.all()
+    return render(request, 'diary/default.html', {'postings':postings})
